e103a27f8a5a53dc15a35283e07eb80aae49d1fa
ez4foxy <foxy.tigrrr@mail.ru>
2024-07-11 18:19:36 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework = 12
+hours = 1.5
+course = "Python"
+time = (hours / homework)
+homework = str(12)
+hours = str(1.5)
+print('Курс: '+ course+ ', всего задач:'+ homework+ ', затрачено часов: '+ hours+ ', среднее время выполнения', time, 'часа.')
